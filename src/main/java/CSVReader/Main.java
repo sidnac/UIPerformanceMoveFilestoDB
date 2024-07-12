4736637818a9780b6470296d38218f8c563b91a4
Sidharth Nachiappan <Sidharth.Nachiappan@dlh.de>
2024-07-12 10:39:11 +0200

    moved MariaDB config details to config file

--- src/main/java/CSVReader/Main.java
+++ src/main/java/CSVReader/Main.java
@@ -12,12 +12,12 @@ public class Main {
 		// Step 2: read file and store them into TestResults objects
 		// Step 3: Write them to SQLite database
 		// Step 4: Write them to MariaDB
-		//DatabaseConnection connectionSQlite = new DatabaseConnectionSQlite();		
+		DatabaseConnection connectionSQlite = new DatabaseConnectionSQlite();		
 		DatabaseConnection connectionMariaDB = new DatabaseConnectionMariaDB();		
 		
 		// first SQlite
-		//CSVReader.setConnectDB(connectionSQlite);
-		//CSVReader.csvReadandWriteDB();
+		CSVReader.setConnectDB(connectionSQlite);
+		CSVReader.csvReadandWriteDB();
 		
 		
 		// second MariaDB
